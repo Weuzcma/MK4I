5ed37c1fc1b1f84e8f861cf3a645310f22deb0a6
Weuzcma <ocamara5283@bronxinternationalhs.com>
2025-03-08 10:08:20 -0500

    Update SwerveDrive.java

--- SwerveDrive.java
+++ SwerveDrive.java
@@ -1,5 +1,5 @@
 package.drive.swerveMP4I;
-mport static edu.wpi.first.hal.FRCNetComm.tInstances.kRobotDriveSwerve_YAGSL;
+import static edu.wpi.first.hal.FRCNetComm.tInstances.kRobotDriveSwerve_YAGSL;
 import static edu.wpi.first.hal.FRCNetComm.tResourceType.kResourceType_RobotDrive;
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.KilogramSquareMeters;
@@ -66,7 +66,45 @@ import swervelib.parser.SwerveDriveConfiguration;
 import swervelib.simulation.SwerveIMUSimulation;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+// Create a trajectory with the Trajectory class.
 
-/**
- * Swerve Drive representing the drive t.
- */
+Trajectory trajectory = TrajectoryGenerator.generateTrajectory(
+    new Pose2d(0, 0, new Rotation2d(0)), 
+    List.of(new Translation2d(1, 1), new Translation2d(2, -1)), 
+    new Pose2d(3, 0, new Rotation2d(Math.PI)), 
+    new TrajectoryConfig(2.0, 2.0) // Vitesse max et accélération
+);
+
+// Use a RamseteController to follow this trajectory.
+RamseteController ramseteController = new RamseteController();
+SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+    new SwerveModulePosition(...), new SwerveModulePosition(...)
+);
+
+// Implementation in robot loop for trajectory tracking.
+Pose2d currentPose = swervePoseEstimator.getEstimatedPosition();
+var ramseteOutput = ramseteController.calculate(currentPose, trajectory.sample(time));
+ChassisSpeeds desiredSpeeds = ramseteOutput;
+Pigeon2Swerve pigeon2 = new Pigeon2Swerve(0);
+pigeon2.setYaw(0); // Reset robot orientation
+Rotation2d currentAngle = pigeon2.getYaw();
+Pose2d correctedPose = new Pose2d(currentPose.getTranslation(), currentAngle);
+Field2d field = new Field2d();
+field.setRobotPose(currentPose);
+SmartDashboard.putData("Field", field);
+
+// Telemetry to display robot speeds.
+SmartDashboard.putNumber("Robot Speed X", chassisSpeeds.vxMetersPerSecond);
+SmartDashboard.putNumber("Robot Speed Y", chassisSpeeds.vyMetersPerSecond);
+SmartDashboard.putNumber("Robot Angular Speed", chassisSpeeds.omegaRadiansPerSecond);
+double joystickX = driverController.getRawAxis(0);  // Joystick X
+double joystickY = driverController.getRawAxis(1);  // Joystick Y
+double joystickRot = driverController.getRawAxis(4);  // Joystick rotation
+ChassisSpeeds chassisSpeeds = new ChassisSpeeds(
+    joystickX * maxSpeed, 
+    joystickY * maxSpeed, 
+    joystickRot * maxAngularSpeed
+);
+List<SwerveModuleState> states = swerveDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+swerveDriveSubsystem.setModuleStates(states);
+0
